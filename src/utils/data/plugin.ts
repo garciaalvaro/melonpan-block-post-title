9edb09084c897de9770e362d1833a6b60115b410
Alvaro <4710040+garciaalvaro@users.noreply.github.com>
2019-08-29 12:06:16 +0200

    Renamed variables.

--- src/utils/data/plugin.ts
+++ src/utils/data/plugin.ts
@@ -2,9 +2,9 @@ const { __ } = wp.i18n;
 
 export const block_category = "melonpan";
 export const plugin_namespace = "melonpan-block";
-export const pr = "mbpt";
-export const plugin_title = __("Melonpan Block - Post Title");
-export const plugin_description = __(
+export const plugin_prefix = "mbpt";
+export const block_title = __("Melonpan Block - Post Title");
+export const block_description = __(
 	"Block that displays the Post Title and Excerpt"
 );
 export const block_name = `${plugin_namespace}/post-title`;
